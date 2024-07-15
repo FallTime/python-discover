b3274885d1e2ddf1afd879cda1f27fc781688625
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2024-07-15 10:30:55 -0300

    fix code - Alterção no arquivo CounterControlledRepetition, ajustes no contador para inicio em zero e uso do contador na divisão para média

--- Codes/Python-How-to-Program/Chapter-3/CounterControlledRepetition.py
+++ Codes/Python-How-to-Program/Chapter-3/CounterControlledRepetition.py
@@ -3,17 +3,15 @@
 
 # initialization phase
 total = 0  # sum of grades
-gradeCounter = 1  # number of grades entered
+gradeCounter = 0  # number of grades entered
 
 # processing phase
-while gradeCounter <= 10:  # loop 10 times
+while gradeCounter < 10:  # loop 10 times
     grade = input("Enter grade: ")  # get one grade
     grade = int(grade)  # convert string to an integer
     total = total + grade  # add up the total
     gradeCounter = gradeCounter + 1  # count 1
 
 # termination phase
-average = total / 10  # integer division
+average = total / gradeCounter  # integer division
 print("Class average is", average)  # output
-
-#
