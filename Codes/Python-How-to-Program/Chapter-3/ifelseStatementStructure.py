960d4f5cbd1009f795280e0984206e85fcefe115
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2024-07-22 10:39:08 -0300

    code - adição do ifelseStatementStructure

---
+++ Codes/Python-How-to-Program/Chapter-3/ifelseStatementStructure.py
@@ -0,0 +1,10 @@
+# Based on Fig. 3.4.
+# if/else double-selection structure flowchart.
+
+# simple structure of if/else statement
+if int(input("Enter grade")) >= 60:
+    print("Passed")  # positive output.
+else:
+    print("Failed")  # negative output.
+
+# have the negative output of the conditional
