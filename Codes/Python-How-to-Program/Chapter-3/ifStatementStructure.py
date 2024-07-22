2f3632671f5e9035fc0ff93a32d813d4acb77168
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2024-07-22 10:37:19 -0300

    code - adição do ifStatementStructure

---
+++ Codes/Python-How-to-Program/Chapter-3/ifStatementStructure.py
@@ -0,0 +1,8 @@
+# Based on Fig. 3.3.
+# if single-selection structure flowchart.
+
+# simple structure of if statement
+if int(input("Enter grade")) >= 60:
+    print("Passed")  # output.
+
+# don't have a negative output in this example.
