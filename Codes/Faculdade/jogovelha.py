b33d6b928e98b3cc3ec89e6033366e4d6e1742b9
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2024-10-09 17:55:29 -0300

    code - refatoração do codigo para o jogador campeão

--- Codes/Faculdade/jogovelha.py
+++ Codes/Faculdade/jogovelha.py
@@ -6,39 +6,102 @@ def cpu_aleatoria():
 
 
 def cpu_campeao():
+    global jogador_turno
     combinacoes_vencedoras = [
         [1, 2, 3], [4, 5, 6], [7, 8, 9],  # Linhas
         [1, 4, 7], [2, 5, 8], [3, 6, 9],  # Colunas
         [1, 5, 9], [3, 5, 7]  # Diagonais
     ]
+    grande_diagonal = [[1, 9], [3, 7]]
+    pequena_diagonal = [[2, 4], [2, 6], [8, 4], [8, 6]]
+    golpe_horizontal = [[6, 1], [6, 7], [4, 3], [4, 9]]
+    golpe_vertical = [[2, 7], [2, 9], [8, 1], [8, 3]]
+    resposta = [1, 3, 7, 9]
+    resposta_h = [2, 8, 2, 8]
+    resposta_v = [4, 6, 4, 6]
 
-    if tabuleiro[5] == 0:
-        return 5
+    # jogando como X
+    if jogador_turno:
+        if tabuleiro[0] >= 3:
+            # prioridade em vitoria
+            for combinacao in combinacoes_vencedoras:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                    for i in combinacao:
+                        if tabuleiro[i] == 0:
+                            return int(i)
+            # caso não tenha vitoria, visa os bloqueios
+            for combinacao in combinacoes_vencedoras:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == -2:
+                    for i in combinacao:
+                        if tabuleiro[i] == 0:
+                            return int(i)
 
-    if tabuleiro[0] >= 3:
-        for combinacao in combinacoes_vencedoras:
-            soma = sum(tabuleiro[i] for i in combinacao)
-            if soma == 2:
-                for i in combinacao:
-                    if tabuleiro[i] == 0:
-                        return int(i)
-            elif soma == -2:
-                for i in combinacao:
-                    if tabuleiro[i] == 0:
-                        return int(i)
+        if tabuleiro[1] == 0:
+            return 1
+        if tabuleiro[9] == 0:
+            return 9
+        if tabuleiro[3] == 0:
+            return 3
+        if tabuleiro[7] == 0:
+            return 7
 
-    if tabuleiro[1] == 0:
-        return 1
-    elif tabuleiro[9] == 0:
-        return 9
-    elif tabuleiro[7] == 0:
-        return 7
-    elif tabuleiro[3] == 0:
-        return 3
+    # jogando como O
     else:
-        for i in range(1, 10):
-            if tabuleiro[i] == 0:
-                return int(i)
+        if tabuleiro[0] >= 3:
+            # prioridade em vitoria
+            for combinacao in combinacoes_vencedoras:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == -2:
+                    for i in combinacao:
+                        if tabuleiro[i] == 0:
+                            return int(i)
+
+            # caso não tenha vitoria, visa os bloqueios
+            for combinacao in combinacoes_vencedoras:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                    for i in combinacao:
+                        if tabuleiro[i] == 0:
+                            return int(i)
+
+            for combinacao in pequena_diagonal:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                        if tabuleiro[resposta[pequena_diagonal.index(combinacao)]] == 0:
+                            return resposta[pequena_diagonal.index(combinacao)]
+
+            for combinacao in golpe_horizontal:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                        if tabuleiro[resposta_h[golpe_horizontal.index(combinacao)]] == 0:
+                            return resposta_h[golpe_horizontal.index(combinacao)]
+
+            for combinacao in golpe_vertical:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                        if tabuleiro[resposta_v[golpe_vertical.index(combinacao)]] == 0:
+                            return resposta_v[golpe_vertical.index(combinacao)]
+                    
+            for combinacao in grande_diagonal:
+                soma = sum(tabuleiro[i] for i in combinacao)
+                if soma == 2:
+                    if tabuleiro[4] == 0:
+                        return 4
+
+        if tabuleiro[5] == 0:
+            return 5
+        if tabuleiro[1] == 0:
+            return 1
+        if tabuleiro[9] == 0:
+            return 9
+        if tabuleiro[3] == 0:
+            return 3
+        if tabuleiro[7] == 0:
+            return 7
+
+
 
 def cpu(escolha):
     match escolha:
@@ -72,7 +135,8 @@ def numero_jogos(quantidade):
 
 
 def resultado_jogo():
-    if tabuleiro[0] == 9:
+    global jogador_vencedor
+    if tabuleiro[0] == 9 and jogador_vencedor == 0:
         print("\nVelha")
         return 0
     elif jogador_vencedor == 1:
@@ -95,9 +159,10 @@ def relatorio_partida(tabuleiro, num_partida, resultado):
 
     add.append(num_partida)
     add.append(tabuleiro.copy())
+    add.append(resultado)
     add.append(contador_vitoria1)
-    add.append(contador_vitoria2)
     add.append(contador_empate)
+    add.append(contador_vitoria2)
     return add
 
 
@@ -108,7 +173,7 @@ def zerar_tabuleiro():
 
 def main(opcao):
     conta = 0
-    global tabuleiro, jogador_turno
+    global tabuleiro, jogador_turno, jogador_vencedor
 
     match opcao:
         # player vs player 
@@ -126,6 +191,7 @@ def main(opcao):
                 relatorio.append(relatorio_partida(tabuleiro, conta, resultado_jogo()))
                 tabuleiro = zerar_tabuleiro()
                 jogador_turno = True
+                jogador_vencedor = 0
 
             return False
         # player vs cpu
@@ -136,13 +202,16 @@ def main(opcao):
             while selecionar_cpu(int(input("Insira a opção:"))):
                 pass
             while conta < partidas:
+                exibirtab(tabuleiro)
                 while verificar_vitoria(tabuleiro) and tabuleiro[0] < 9:
-                    exibirtab(tabuleiro)
+
                     if jogador_turno:
                         if jogada(tabuleiro):
+                            exibirtab(tabuleiro)
                             tabuleiro[0] += 1
                     else:
                         if jogada_cpu(tabuleiro, cpu(cpu_escolha)):
+                            exibirtab(tabuleiro)
                             tabuleiro[0] += 1
 
                 conta += 1
@@ -150,6 +219,7 @@ def main(opcao):
                 relatorio.append(relatorio_partida(tabuleiro, conta, resultado_jogo()))
                 tabuleiro = zerar_tabuleiro()
                 jogador_turno = True
+                jogador_vencedor = 0
 
             return False
         # cpu vs cpu
@@ -168,20 +238,22 @@ def main(opcao):
             print(cpu_escolha2)
 
             while conta < partidas:
+                exibirtab(tabuleiro)
                 while verificar_vitoria(tabuleiro) and tabuleiro[0] < 9:
-                    exibirtab(tabuleiro)
                     if jogador_turno:
                         if jogada_cpu(tabuleiro, cpu(cpu_escolha1)):
+                            exibirtab(tabuleiro)
                             tabuleiro[0] += 1
                     else:
                         if jogada_cpu(tabuleiro, cpu(cpu_escolha2)):
+                            exibirtab(tabuleiro)
                             tabuleiro[0] += 1
 
                 conta += 1
-                exibirtab(tabuleiro)
                 relatorio.append(relatorio_partida(tabuleiro, conta, resultado_jogo()))
                 tabuleiro = zerar_tabuleiro()
                 jogador_turno = True
+                jogador_vencedor = 0
 
             return False
         # default case
@@ -245,7 +317,6 @@ def jogada_cpu(tabuleiro, casa):
             jogador_turno = True
         return True
     else:
-        print("\nCasa indisponível")
         return False
 
 
